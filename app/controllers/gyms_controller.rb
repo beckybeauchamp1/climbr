943ca2dd9d5ecb33f42cf263888e734d4f012483
Charles <charlesthwang@gmail.com>
2016-03-18 15:08:11 -0400

    created gyms controller and gyms view pages

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,3 +1,21 @@
 class GymsController < ApplicationController
 
+  def index
+    @gyms = Gym.all
+  end
+
+  def new
+    @gym = Gym.new
+  end
+
+  def create
+    @gym = Gym.create!(gym_params)
+    redirect_to gyms_path(@gym)
+  end
+
+  private
+    def gym_params
+      params.require(:gym).permit(:name, :location)
+    end
+
 end
